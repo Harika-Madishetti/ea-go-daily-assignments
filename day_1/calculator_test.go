d348d84e395657e8ff02546123a88369606c0da0
Harika-Madishetti <r121662@rguktrkv.ac.in>
2022-12-22 17:14:30 +0530

    Add various test scenarios for Addition

--- day_1/calculator_test.go
+++ day_1/calculator_test.go
@@ -7,6 +7,20 @@ import (
 func TestSuccessfulAdditionOfTwoPositiveNumbers(t *testing.T) {
 	result := add(10, 2)
 	if result != 12 {
-		t.Error("wrong addition")
+		t.Error("Addition Failed")
+	}
+}
+
+func TestSuccessfulAdditionOfTwoNegativeNumbers(t *testing.T) {
+	result := add(-12, -10)
+	if result != -22 {
+		t.Error("Addition Failed")
+	}
+}
+
+func TestSuccessfulAdditionOfTwoIntegers(t *testing.T) {
+	result := add(-10, 10)
+	if result != 0 {
+		t.Error("Addition Failed")
 	}
 }
